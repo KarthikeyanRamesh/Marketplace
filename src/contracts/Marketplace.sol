c6f1b5a29c881527748472fff6e2e391fcec272b
Karthikeyan Ramesh <karthisujatha10@gmail.com>
2020-06-28 11:24:39 +0530

    Marketplace Application

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -9,7 +9,7 @@ contract Marketplace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -17,7 +17,15 @@ contract Marketplace {
         uint id,
         string name,
         uint price,
-        address owner,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased (
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
         bool purchased
     );
 
@@ -28,7 +36,7 @@ contract Marketplace {
     function createProduct(string memory _name, uint _price) public {
         //Require a valid name 
 
-        require(bytes(name).length > 0);
+        require(bytes(_name).length > 0);
 
         //Require a valid price
 
@@ -45,6 +53,34 @@ contract Marketplace {
         //Trigger an event
 
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+
+
+    function purchaseProduct(uint _id) public payable {
+        //fetch the product
+        Product memory _product = products[_id];
+        //fetch the owner
+        address payable _seller = _product.owner;
+        //make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        //Require that there is enough ether in the transaction
+        require(msg.value >= _product.price);
+        //Require that the product has not purchased already
+        require(! _product.purchased);
+        //Require that the buyer is not the seller
+        require(_seller != msg.sender); 
+        // transfer ownership to the buyer
+        _product.owner = msg.sender;
+        //Mark as purchased
+        _product.purchased = true;
+        //Update the product
+        products[_id] = _product;
+        //Pay the seller by sending them ether
+        address(_seller).transfer(msg.value);
+        //trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+
     }
 
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,5 +1,8 @@
 const Marketplace = artifacts.require('./Marketplace.sol')
 
+require('chai')
+.use(require('chai-as-promised'))
+.should()
 
 contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
@@ -32,6 +35,7 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
 
         before(async () => {
             result = await marketplace.createProduct('IPhone X', web3.utils.toWei('1', 'Ether'), {from : seller})
+            console.log(result)
             productCount = await marketplace.productCount()
         })
 
@@ -46,24 +50,63 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
             assert.equal(event.purchased, false, 'purchase is correct')
        
        
-            //FAILURE CASE
+            //FAILURE CASE: product must have a name
+            await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from : seller}).should.be.rejected;
+            await await marketplace.createProduct('IPhone X', 0, {from : seller}).should.be.rejected;
 
+        })
+
+
+        it('lists products', async() => {
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(product.name, 'IPhone X', 'name is correct')
+            assert.equal(product.price, '1000000000000000000', 'price is correct')
+            assert.equal(product.owner, seller, 'owner is correct')
+            assert.equal(product.purchased, false, 'purchased is correct')
+        })
+
+        it('sells products', async() => {
+            //Track the seller balance before purchase
+            let oldSellerBalance
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+            //SUCCESS buyer makes purchase
+            result = await marketplace.purchaseProduct(productCount,{from: buyer, value: web3.utils.toWei('1', 'ether')})
             
+            //Check logs
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct');
+            assert.equal(event.name, 'IPhone X', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, buyer, 'is correct')
+            assert.equal(event.purchased, true, 'purchase is correct')
+            //Check the seller balance after the transaction
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+
+
+            const expectedBalance = oldSellerBalance.add(price)
+            assert.equal(newSellerBalance.toString(), expectedBalance.toString())
+
+            //FAILURE: Tries to buy a product that is not exist
+            await marketplace.purchaseProduct(99, {from: buyer, value: web3.utils.toWei('1', 'ether')}).should.be.rejected;
+            //FAILURE: Buyer tries to buy without enough ether
+            await marketplace.purchaseProduct(productCount,{from: buyer, value: web3.utils.toWei('0.5', 'ether')}).should.be.rejected;
+            //FAILURE: deployer tries to buy the product, i.e, product can't be purchased twice
+            await marketplace.purchaseProduct(productCount,{from: deployer, value: web3.utils.toWei('1', 'ether')}).should.be.rejected;
+            //FAILURE: Buyer tries to buy again, i.e., buyer can't be the seller
+            await marketplace.purchaseProduct(productCount,{from: buyer, value: web3.utils.toWei('1', 'ether')}).should.be.rejected;
+
+
         })
 
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
